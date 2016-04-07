bb689b6fd380b6b0f1d1acfc1668bf3fee5d893f
scotthanton <scott_hanton94@hotmail.com>
2016-04-07 09:10:43 -0400

    Update 20160310160010_create_prerequisites.rb

--- db/migrate/20160310160010_create_prerequisites.rb
+++ db/migrate/20160310160010_create_prerequisites.rb
@@ -2,8 +2,8 @@ class CreatePrerequisites < ActiveRecord::Migration
   def change
     create_table :prerequisites do |t|
       t.integer :skill_id
-      t.integer :prerequisite_skill_id
       t.integer :level_id
+      t.integer :prerequisite_skill_id
       t.timestamps null: false
     end
   end
