eaa3df45f60b398ac14784061ffb515030e753a4
scotthanton <scott_hanton94@hotmail.com>
2016-04-07 09:09:04 -0400

    Update 20160310160010_create_prerequisites.rb

--- db/migrate/20160310160010_create_prerequisites.rb
+++ db/migrate/20160310160010_create_prerequisites.rb
@@ -3,7 +3,7 @@ class CreatePrerequisites < ActiveRecord::Migration
     create_table :prerequisites do |t|
       t.integer :skill_id
       t.integer :prerequisite_skill_id
-
+      t.integer :level_id
       t.timestamps null: false
     end
   end
